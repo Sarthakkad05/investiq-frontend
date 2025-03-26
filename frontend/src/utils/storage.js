9ad69e0e303a4a40aadfb13ed93e9aa298b6c78f
Sarthak Kad <kadsarthak33@gmail.com>
2025-03-26 22:09:56 +0530

    Delete frontend directory

--- frontend/src/utils/storage.js
+++
@@ -1,3 +0,0 @@
-export const setToken = (token) => localStorage.setItem("idToken", token);
-export const getToken = () => localStorage.getItem("idToken");
-export const removeToken = () => localStorage.removeItem("idToken");

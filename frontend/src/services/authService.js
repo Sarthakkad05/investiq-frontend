b5c58604f7323349f7650f9e221adaa2fc0b50c4
Sarthak Kad <kadsarthak33@gmail.com>
2025-03-27 12:20:47 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "https://invest-deploy-3.onrender.com";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const signup = async (email, password, display_name) => {
-  return await axios.post(`${API_URL}/signup`, { email, password, display_name });
+  return await axios.post(`${BASE_URL}/signup`, { email, password, display_name });
 };
 
 export const login = async (email, password) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
+  const response = await axios.post(`${BASE_URL}/login`, { email, password });
   console.log(response.data);
   return response.data;
   

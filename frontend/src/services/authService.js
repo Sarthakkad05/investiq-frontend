ae0a8a4fe1b56dd03dc93bc4ee9c1826ce516d0a
Sarthak Kad <kadsarthak33@gmail.com>
2025-03-27 12:39:25 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.BACKEND_URL;
 console.log("Backend URL:", BASE_URL); 
 
 export const signup = async (email, password, display_name) => {

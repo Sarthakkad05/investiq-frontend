e0add9fb84aeb69e3cbb499d48d5b3dbcbcfc11a
Sarthak Kad <kadsarthak33@gmail.com>
2025-04-01 20:10:21 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://your-backend.onrender.com";
+const BASE_URL = "https://invest-deploy-3.onrender.com";
 console.log("Backend URL:", BASE_URL); 
 
 export const signup = async (email, password, display_name) => {
